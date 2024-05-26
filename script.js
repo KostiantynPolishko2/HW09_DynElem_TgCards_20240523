9cfc75782fdceb7c95a8c800956075024c120a15
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-05-26 19:24:21 +0300

    9 Created function layoutGroupChat

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
     console.log('Start');
 
     const cardChat = createGroupChat(document.querySelector('main#chat'));
-    cardChat(dictTxts.values());
+    cardChat(dictTxts);
 })
 
 const createTag = (nameElement, nameStyle) => {
@@ -29,29 +29,26 @@ const createTagDict = (nameTag, classNames) => {
     return dictElement;
 }
 
-const setImgAttr = (elemtn, src, alt = 'logo') => {
+const setImgAttr = (element, src, alt = 'logo') => {
     element.setAttribute('src', src);
     element.setAttribute('alt', alt);
 }
 
 const createGroupChat = (element) => {
-    const divTagClassNames = ['item_photo', 'item_txt', 'item_meme_name', 'item_flex_row', 'item_meme_date', 'chat_photo', 'msg_count'];
-    const pTagClassNames = dictTxts.keys();
-    const imgTagClassNames = attrImgs.keys();
-
-    // console.log(element);
+    const divTagClassNames = ['item_photo', 'item_txt', 'item_meme_name', 'item_meme_description', 'item_flex_row', 'item_meme_date', 'chat_photo', 'msg_count'];
+    const pTagClassNames = Array.from(dictTxts.keys());
+    const imgTagClassNames = Array.from(attrImgs.keys());
 
     return (dictTxts) => {
 
-        console.log(dictTxts);
-
         const dictDivs = createTagDict('div', divTagClassNames);
         dictDivs.get('item_meme_name').className += ' item_flex_row space_between';
+        dictDivs.get('item_meme_description').className += ' item_flex_row';
 
         const dictP = createTagDict('p', pTagClassNames);
         const dictImg = createTagDict('img', imgTagClassNames);
 
-        for(let  _className of imgTagClassNames){
+        for(let _className of imgTagClassNames){
             setImgAttr(dictImg.get(_className), attrImgs.get(_className));
         }
 
@@ -60,9 +57,25 @@ const createGroupChat = (element) => {
         insertTxtNode(icon_volume_off, 'volume_off');
 
         for(_className of pTagClassNames){
-            insertTxtNode(dictp.get(_className), dictTxts.get(_className));
+            insertTxtNode(dictP.get(_className), dictTxts.get(_className));
         }
 
-        
+        layoutGroupChat(element, dictDivs, dictP, dictImg, icon_volume_off);
     }
+}
+
+const layoutGroupChat = (element, dictDivs, dictP, dictImg, icon_volume_off) => {
+    let sectionGroupChat = createTag('section', 'group_chat');
+
+    element.appendChild(sectionGroupChat);
+    sectionGroupChat.appendChild(dictDivs.get('item_photo')).appendChild(dictImg.get('img_item_photo'));
+    sectionGroupChat.appendChild(dictDivs.get('item_txt')).appendChild(dictDivs.get('item_meme_name')).appendChild(dictDivs.get('item_meme_description'));
+    dictDivs.get('item_meme_description').appendChild(dictP.get('txt_meme'));
+    dictDivs.get('item_meme_description').appendChild(icon_volume_off);
+    dictDivs.get('item_meme_name').appendChild(dictP.get('txt_time'));
+
+    dictDivs.get('item_txt').appendChild(dictDivs.get('item_meme_date')).appendChild(dictDivs.get('item_flex_row')).appendChild(dictDivs.get('chat_photo'));
+    dictDivs.get('chat_photo').appendChild(dictImg.get('img_chat_foto'));
+    dictDivs.get('item_flex_row').appendChild(dictP.get('txt_img_type'));
+    dictDivs.get('item_meme_date').appendChild(dictDivs.get('msg_count')).appendChild(dictP.get('txt_msg_count'));
 }
